91a423485f0ceafdd48abb818f607b673b9286a9
Jose <93582239+J0sePonce@users.noreply.github.com>
2023-05-20 13:43:00 +0200

    Update script.sh
    Update Docker installation according to the Docker page installation, the script has been made shorter. Poco más

--- wordpress/certs/script.sh
+++ wordpress/certs/script.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-
 # Desinstalar versiones antiguas de Docker
 apt-get remove docker docker-engine docker.io containerd runc -y
 
 # Añadir repositorio Docker
-apt-get update
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    -y
+apt-get update && apt-get install ca-certificates curl gnupg -y
 
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Instalar Docker Engine
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io -y
-
-# Instalar Docker Compose
-apt install docker-compose -y
+# Instalar Docker Engine y Docker Compose
+apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
 
 # Genera los certificados autofirmados
-openssl genrsa -out nginx.key 2048
+openssl genrsa -out nginx.key 4096
 openssl req -new -key nginx.key -out nginx.csr
 openssl x509 -req -in nginx.csr -signkey nginx.key -out nginx.crt
 
@@ -45,8 +35,7 @@ mv "$file_path" "$destination"
 docker-compose pull
 
 # Reinicia el servicio Docker para que haga efecto la configuracion del fichero daemon.json
-service docker stop
-service docker start
+service docker stop && service docker start
 
 # Crea las carpetas necesarias y cambia los permisos para la carpeta del volumen de Grafana, ya que no lo hace automaticamente
 mkdir ../grafana_data && mkdir ../prometheus_data
